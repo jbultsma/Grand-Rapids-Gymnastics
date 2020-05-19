a753835d45b2b0bfabe777772cc066749c4ecc3d
jbultsma <joshuabultsma@gmail.com>
2020-05-19 15:09:53 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/grandrapidsgymnastics/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.grandrapidsgymnastics.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
